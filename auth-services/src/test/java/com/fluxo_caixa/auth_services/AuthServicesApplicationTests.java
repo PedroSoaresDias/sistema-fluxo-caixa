a42d21dcfa842a94bd28a5d30c0f550dfd5508d8
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-10-08 13:49:04 -0300

    First commit

---
+++ auth-services/src/test/java/com/fluxo_caixa/auth_services/AuthServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxo_caixa.auth_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cash-flow-services/src/test/java/com/fluxo_caixa/cash_flow_services/CashFlowServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxo_caixa.cash_flow_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
