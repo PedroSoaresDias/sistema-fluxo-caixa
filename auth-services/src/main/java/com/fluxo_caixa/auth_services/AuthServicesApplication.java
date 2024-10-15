1b91af67f302a16d559a05bd2fd7864dc1c5cc1d
Pedro Soares Dias <pedroferraridias@gmail.com>
2024-10-15 10:39:57 -0300

    Configurando o Dotenv para rodar localmente e no Docker e ajustando o docker-compose.yml

--- auth-services/src/main/java/com/fluxo_caixa/auth_services/AuthServicesApplication.java
+++ auth-services/src/main/java/com/fluxo_caixa/auth_services/AuthServicesApplication.java
@@ -11,7 +11,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class AuthServicesApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv;
+
+        if (System.getenv("DOCKER_ENV") != null) {
+            dotenv = Dotenv.configure()
+                .directory("/app")
+                .load();
+        } else {
+            dotenv = Dotenv.configure()
+                .directory("./")
+                .load();
+        }
+
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 		SpringApplication.run(AuthServicesApplication.class, args);
