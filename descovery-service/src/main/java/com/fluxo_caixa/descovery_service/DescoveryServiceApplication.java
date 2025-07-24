347b32941fa4bfd1239c8e64efa16eba1a5c13c4
Pedro Soares Dias <pedroferraridias@gmail.com>
2025-07-24 10:44:01 -0300

    Criando o projeto de descoberta de serviços e API Gateway

---
+++ descovery-service/src/main/java/com/fluxo_caixa/descovery_service/DescoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fluxo_caixa.descovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescoveryServiceApplication.class, args);
+	}
+
+}
---
+++ descovery-service/src/test/java/com/fluxo_caixa/descovery_service/DescoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxo_caixa.descovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
